6e74a4394ef6cc122c1b0b539057406178e9e68a
Florian Sjouw <f.sjouw@recreate.nl>
2020-11-01 15:20:58 +0100

    screenshots

---
+++ Assets/Scripts/ScreenCaptureUtility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCaptureUtility : MonoBehaviour
+{
+    [SerializeField]
+    string path = "";
+
+    [ContextMenu("makeScreenshot")]
+    public void makeScreenshot()
+    {
+        ScreenCapture.CaptureScreenshot(path);
+    }
+}
